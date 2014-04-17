83e23cf8743347c16ea00e28b4c44ee1f4e756c7
unknown <zshaikh@US-507-05.amsacs.org>
2014-04-17 13:09:20 -0400

    1st Commit: Player Movement and Networking semi successful

---
+++ Assets/Person.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Person : MonoBehaviour {
+	public bool alive = true;
+	CharacterController c;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!alive) {
+			this.gameObject.renderer.enabled = false;			
+		}
+	}
+
+	void Movement()
+	{
+
+	}
+}
---
+++ Assets/Player/PlayerMove.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : Person {
+	//CharacterController c;
+	public Vector3 newPos = new Vector3(-39.68812f, -39.15045f, -0.587138f);
+	
+	void Start () {
+		//c = transform.GetComponent<CharacterController> ();
+		newPos.x = transform.localPosition.x;
+
+	}
+
+	void Update () {
+		if (Input.GetAxis ("Horizontal") == 1) {
+			newPos.x += 0.25f;		
+		}
+		if (Input.GetAxis ("Horizontal") == -1) {
+			newPos.x -= 0.25f;
+		}
+		if (Input.GetAxis ("Vertical") == 1) {
+			newPos.y += 0.25f;		
+		}
+		if (Input.GetAxis ("Vertical") == -1) {
+			newPos.y -= 0.25f;
+		}
+		//c.Move (newPos);
+		transform.localPosition = newPos;
+	
+	}
+}
