12fb37cd36686fc669a972bdc039680352f3ff8a
unknown <zshaikh@US-507-05.amsacs.org>
2014-05-01 08:25:25 -0400

    Fixed some issues regarding prefabs

--- Thoracic Laceration/Assets/Enemy/EnemyMovement.cs
+++ Thoracic Laceration/Assets/Enemy/EnemyMovement.cs
@@ -6,16 +6,29 @@ public class NewBehaviourScript : MonoBehaviour {
 	private Vector3 newPos;
 	public float speed = 35f;
 	CharacterController c;
+	Vector3 closestPos;
 	Vector3 lastPosition;
 	float minimumMovement = .05f;
-
+	GameObject[] nodes;
 	// Use this for initialization
 	void Start () {
 		c = transform.GetComponent<CharacterController> ();
 	}
 
+	Vector3 diffPos;
+	Vector3 beforePos;
 	// Update is called once per frame
 	void Update () {
+
+		nodes = GameObject.FindGameObjectsWithTag("Node");
+
+	/*	foreach (GameObject n in nodes) {
+
+			diffPos = n.transform.position - this.transform.position;
+			diffPos *= diffPos;
+
+
+		}*/
 
 		c.Move(newPos);
 		
