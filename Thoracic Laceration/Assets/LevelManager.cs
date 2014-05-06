3d92b7d5afe1ba473440d0b3217226d00d696ee2
unknown <jpalmieri@US-507-23.amsacs.org>
2014-05-06 13:02:43 -0400

    Added a tutorial manager, the ability to go from one room to another, and more rooms on floor 1.

---
+++ Thoracic Laceration/Assets/LevelManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	public int currentLevel;
+
+	// Use this for initialization
+	void Start () {
+		currentLevel = Application.loadedLevel;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter (Collider other) {
+		Application.LoadLevel (currentLevel + 1);
+	}
+}
