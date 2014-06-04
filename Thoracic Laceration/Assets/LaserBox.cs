414bfcec9c5049d8afa9afc477b3c9d661a8f848
unknown <jpalmieri@US-507-23.amsacs.org>
2014-06-04 09:29:39 -0400

    Added Lasers coming back on after being deactivated

--- Thoracic Laceration/Assets/LaserBox.cs
+++ Thoracic Laceration/Assets/LaserBox.cs
@@ -2,9 +2,13 @@
 using System.Collections;
 
 public class LaserBox : MonoBehaviour {
+	GameObject[] g;
+	int counter = 0;
 
 	// Use this for initialization
 	void Start () {
+		g = GameObject.FindGameObjectsWithTag ("Laser");
+
 	
 	}
 	
@@ -12,4 +16,17 @@ public class LaserBox : MonoBehaviour {
 	void Update () {
 	
 	}
+
+	void OnTriggerEnter(Collider other) {
+		for (int i = 0; i < g.Length; i++) {
+			g[i].SetActive(false);
+		}
+		StartCoroutine (ReturnAfterSec());
+	}
+	IEnumerator ReturnAfterSec(){
+		yield return new WaitForSeconds (5f);
+		for (int i = 0; i < g.Length; i++) {
+			g[i].SetActive(true);
+	}
+}
 }
